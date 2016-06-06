35055d4d704bc27fbb850c39e673c837c4218f47
ifdion <ifdion@gmail.com>
2016-06-07 06:39:44 +0700

    add missing export class name
    Add the name 'Article' to the export statement.

--- 2-react-router/src/js/components/Article.js
+++ 2-react-router/src/js/components/Article.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class extends React.Component {
+export default class Article extends React.Component {
   render() {
     const { title } = this.props;
 
