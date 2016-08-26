d0752f66bdbb62035270c671299e39c10817b4f5
Will Stern <willrstern@gmail.com>
2016-08-25 21:02:52 -0500

    small tweaks

--- 6-mobx-react/src/js/TodoList.js
+++ 6-mobx-react/src/js/TodoList.js
@@ -24,11 +24,13 @@ export default class TodoList extends React.Component {
 
     const todoLis = filteredTodos.map(todo => (
       <li key={todo.id}>
-       <input type="checkbox" onChange={this.toggleComplete.bind(this, todo)} value={todo.complete} checked={todo.complete} />{todo.value}</li>
+       <input type="checkbox" onChange={this.toggleComplete.bind(this, todo)} value={todo.complete} checked={todo.complete} />
+       <span>{todo.value}</span>
+      </li>
     ))
     return <div>
       <h1>todos</h1>
-      <input className="create" onKeyPress={this.createNew.bind(this)} />
+      <input className="new" onKeyPress={this.createNew.bind(this)} />
       <input className="filter" value={filter} onChange={this.filter.bind(this)} />
       <ul>{todoLis}</ul>
       <a href="#" onClick={clearComplete}>Clear Complete</a>
