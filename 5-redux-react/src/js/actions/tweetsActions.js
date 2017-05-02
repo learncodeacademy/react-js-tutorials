14c11ad4e349f11500a0701723839959d5c23c8e
Will Stern <willrstern@gmail.com>
2017-05-02 12:55:51 -0500

    Update tweetsActions.js

--- 5-redux-react/src/js/actions/tweetsActions.js
+++ 5-redux-react/src/js/actions/tweetsActions.js
@@ -6,7 +6,7 @@ export function fetchTweets() {
       http://rest.learncode.academy is a public test server, so another user's experimentation can break your tests
       If you get console errors due to bad data:
       - change "reacttest" below to any other username
-      - post some tweets to http://rest.learncode.academy/api/yourusername
+      - post some tweets to http://rest.learncode.academy/api/yourusername/tweets
     */
     axios.get("http://rest.learncode.academy/api/reacttest/tweets")
       .then((response) => {
