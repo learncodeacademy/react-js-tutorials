8f1d3142e984d6e18ad4fef09da46c2daff3b5d1
Will Stern <willrstern@gmail.com>
2017-05-02 12:54:37 -0500

    Update tweetsActions.js

--- 5-redux-react/src/js/actions/tweetsActions.js
+++ 5-redux-react/src/js/actions/tweetsActions.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 export function fetchTweets() {
   return function(dispatch) {
-    axios.get("http://rest.learncode.academy/api/test123/tweets")
+    /* 
+      http://rest.learncode.academy is a public test server, so another user's experimentation can break your tests
+      If you get console errors due to bad data:
+      - change "reacttest" below to any other username
+      - post some tweets to http://rest.learncode.academy/api/yourusername
+    */
+    axios.get("http://rest.learncode.academy/api/reacttest/tweets")
       .then((response) => {
         dispatch({type: "FETCH_TWEETS_FULFILLED", payload: response.data})
       })
