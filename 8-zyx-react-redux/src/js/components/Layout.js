b66b3ed0e1048c7582dba5d69c2d0b791dff685c
zyxmn <zyxmndaleyjes@gmail.com>
2018-07-01 18:03:19 +0100

    display rest of movies

--- 8-zyx-react-redux/src/js/components/Layout.js
+++ 8-zyx-react-redux/src/js/components/Layout.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { connect } from "react-redux"
-
 import { fetchTopRatedMovies } from "../actions/imdbActions"
+import singleMovie from "./singleMovie"
+
 
 @connect((store) => {
   return {
@@ -9,6 +10,7 @@ import { fetchTopRatedMovies } from "../actions/imdbActions"
     fetchedTopRatedMovies: store.imdb.fetchedTopRatedMovies
   };
 })
+
 export default class Layout extends React.Component {
   componentWillMount() {
     this.props.dispatch(fetchTopRatedMovies())
@@ -16,7 +18,13 @@ export default class Layout extends React.Component {
 
   render() {
     const {topRatedMovies} = this.props;
-    const mappedTopRatedMovies = topRatedMovies.map(topRatedMovie => <li>{topRatedMovie.title}</li>)
+    const mappedTopRatedMovies = topRatedMovies.map(topRatedMovie =>
+	    <li>
+	     <img src={"https://image.tmdb.org/t/p/w500" + topRatedMovie.poster_path}></img>
+	     <h6>{topRatedMovie.title}</h6>
+	     <h6>{topRatedMovie.overview}</h6>
+	     <h6>{topRatedMovie.release_date}</h6>
+	    </li>);
     return <div>
       <h1>Top Rated Movies</h1>
       <ul>{mappedTopRatedMovies}</ul>
